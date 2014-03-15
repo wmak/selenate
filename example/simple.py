6698c1427eade6299863fe987ad8d6d3e3b78629
wmak <william.mak@hotmail.ca>
2014-03-15 16:45:54 -0400

    deleting example for now, will add when ready

--- example/simple.py
+++
@@ -1,7 +0,0 @@
-import selenate
-
-if __name__=="__main__":
-    driver = selenate.Selenate()
-    driver.get("https://github.com/wmak/selenate")
-    driver.type_to("id=js-command-bar-field", "Hello World")
-    driver.quit()
