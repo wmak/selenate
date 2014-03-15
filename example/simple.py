02fc1f378abafc51559e685cd196f483c29d7628
wmak <william.mak@hotmail.ca>
2014-03-15 00:03:26 -0400

    classes should be capitalized

--- example/simple.py
+++ example/simple.py
@@ -1,7 +1,7 @@
 import selenate
 
 if __name__=="__main__":
-    driver = selenate.selenate()
+    driver = selenate.Selenate()
     driver.get("https://github.com/wmak/selenate")
     driver.type_to("id=js-command-bar-field", "Hello World")
     driver.quit()
