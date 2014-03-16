ca58fd69e7a3e01f2ce5242591c418113ae9ba45
wmak <william.mak@hotmail.ca>
2014-03-16 15:59:39 -0400

    fixing import issue.

---
+++ selenate/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'selenate'
+__version__ = '0.1.1'
+__build__ = 0x000101
+__author__ = 'William Mak'
+__copyright__ = 'Copyright 2014 William Mak'
+
+from .selenate import Selenate
--- setup.py
+++ setup.py
@@ -2,7 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="selenate",
-    version="0.1",
+    version="0.1.1",
+    author="William Mak",
     packages=find_packages(exclude=['*.tests']),
     test_suite="selenate.tests",
-    )
+    description="Web Automation made easy",
+    install_requires=[
+        "selenium >= 2.40.0",
+    ],
+)
