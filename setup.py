5a5a226135a4cec6121215ed2520d2cb451864e7
wmak <william.mak@hotmail.ca>
2014-03-17 00:17:13 -0400

    setup update

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="selenate",
-    version="0.1.1",
+    version="0.1.2",
     author="William Mak",
+    url="https://github.com/wmak/selenate",
     packages=find_packages(exclude=['*.tests']),
     test_suite="selenate.tests",
     description="Web Automation made easy",
